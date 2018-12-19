f470652c0b8a3183f6919d1536ad9b50a77057a0
kimk <38691828+claudiact6@users.noreply.github.com>
2018-12-18 20:09:28 -0600

    scraped articles displayed with handlebars

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,5 +1,16 @@
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    res.render("index.handlebars");
+var axios = require("axios");
+
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    var articlesHB;
+    axios.get("http://localhost:3000/api/articles").then(function(response) {
+      articlesHB = {
+        articles: response.data
+      };
+      res.render("index.handlebars", articlesHB);
+    })
+    .catch(function(error) {
+      console.error(error.stack);
+    });
   });
 };
